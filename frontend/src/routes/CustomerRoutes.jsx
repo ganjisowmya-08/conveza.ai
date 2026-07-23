1410cf5b23dd26212e710423ff027524e6d8fdb5
ganjisowmya-08 <ganjisowmya08@gmail.com>
2026-07-23 16:03:28 +0530

    feat: implement marketing layout, customer routing configuration, and user sign-up page

--- frontend/src/routes/CustomerRoutes.jsx
+++ frontend/src/routes/CustomerRoutes.jsx
@@ -1,19 +1,36 @@
 import { Routes, Route, Navigate } from "react-router-dom";
-import Home from "../pages/marketing/Home";
+import Hero from "../pages/marketing/Home/Hero";
+import Features from "../pages/marketing/Home/Features";
+import Solutions from "../pages/marketing/Home/Solutions";
+import Pricing from "../pages/marketing/Home/Pricing";
+import Customers from "../pages/marketing/Home/Customers";
+import Resources from "../pages/marketing/Home/Resources";
+import MarketingLayout from "../layouts/MarketingLayout";
 import Login from "../pages/auth/Login";
 import Signup from "../pages/auth/Signup";
+import SignIn from "../pages/auth/SignIn";
+import SignUp from "../pages/auth/SignUp";
 import CustomerLayout from "../layouts/customerLayout";
 import Dashboard from "../pages/customer/dashboard";
 
 export default function CustomerRoutes() {
     return (
         <Routes>
-            {/* Marketing Landing Page */}
-            <Route path="/" element={<Home />} />
+            {/* Marketing Pages with Navbar */}
+            <Route element={<MarketingLayout />}>
+                <Route path="/" element={<Hero />} />
+                <Route path="/features" element={<Features />} />
+                <Route path="/solutions" element={<Solutions />} />
+                <Route path="/pricing" element={<Pricing />} />
+                <Route path="/customers" element={<Customers />} />
+                <Route path="/resources" element={<Resources />} />
+            </Route>
 
             {/* Auth Routes */}
             <Route path="/login" element={<Login />} />
+            <Route path="/signin" element={<SignIn />} />
             <Route path="/signup" element={<Signup />} />
+            <Route path="/register" element={<SignUp />} />
 
             {/* Customer Workspace */}
             <Route path="/customer" element={<CustomerLayout />}>
