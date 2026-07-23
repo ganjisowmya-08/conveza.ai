1d652e988333b56d1d2017aab3de8e7b0aaaeaf5
ganjisowmya-08 <ganjisowmya08@gmail.com>
2026-07-23 13:59:56 +0530

    feat: initialize frontend project structure and boilerplate assets

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+    return (
+        <div>
+            <h1 style={{ fontSize: "4rem" }}>Conveza.ai</h1>
+            <p>Marketing Website is Working 🚀</p>
+        </div>
+    );
+}
+
+export default App;
